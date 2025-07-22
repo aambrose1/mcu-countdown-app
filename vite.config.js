cd09613f5a23a820d23444f016094e3ee20b572b
Andrea Ambrose <andreamoesha@gmail.com>
2025-07-21 23:17:29 -0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: 'index.html',
-        },
-    },
   base: '/mcu-countdown-app/',
 })
